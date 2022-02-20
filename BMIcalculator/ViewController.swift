e4788ffe1836c869d46c8aac58a452c2f353e648
ayerose <reya-k@outlook.de>
2022-02-20 14:40:51 +0000

    Initial Commit

---
+++ BMIcalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMIcalculator
+//
+//  Created by Soreya Koura on 20.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
