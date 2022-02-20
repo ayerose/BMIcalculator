f44466234caf71db50fff306db5936fe4ee4c096
ayerose <reya-k@outlook.de>
2022-02-20 16:09:15 +0000

    BMIcalculator

--- BMIcalculator/ViewController.swift
+++ BMIcalculator/ViewController.swift
@@ -8,12 +8,101 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlet
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var sizeTextField: UITextField!
+    
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    
+    @IBOutlet weak var enterButton: UIButton!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addTargetToTextField()
+        
+        // disable enter btn and set color to grey on load
+        enterButton.backgroundColor = UIColor(white: 0.8, alpha: 0.2)
+        enterButton.isEnabled = false
     }
+    // MARK: Actions / func
+    
+    
+    @IBAction func clickHereButton_Tapped(_ sender: Any) {
+    }
+    
+    func calculateBMI() {
+        
+        
 
-
+    }
+    
+    func addTargetToTextField(){
+        // call func textFieldChanged everytime when editing in textfield changes (when user types or changes something)
+        nameTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+        sizeTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+        weightTextField.addTarget(self, action: #selector(textFieldChanged), for: UIControl.Event.editingChanged)
+        
+    }
+    @objc func textFieldChanged() {
+        // check if every textfield has input
+        if !(nameTextField.text!.isEmpty) && !(sizeTextField.text!.isEmpty ) && !(weightTextField.text!.isEmpty ){
+            // set enter btn color to white and enter btn is clickable
+            enterButton.backgroundColor = UIColor(white: 1.0, alpha: 0.8)
+            enterButton.isEnabled = true
+            
+        } else {
+            // grey out btn and not clickable
+            enterButton.backgroundColor = UIColor(white: 0.8, alpha: 0.2)
+            enterButton.isEnabled = false
+        }
+        
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
